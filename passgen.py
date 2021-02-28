1ee251b5d3f3d3038be4dc3d5331efc985d45156
Tony Chen <git@tonychen.page>
2021-02-28 17:59:55 +0800

    Switch to String constants

--- passgen.py
+++ passgen.py
@@ -3,14 +3,14 @@
 from argparse import ArgumentParser
 from secrets import choice
 from pyperclip import copy
+import string
 
 parser = ArgumentParser(description="A secure random password generator!")
 
-NUMBER = "1234567890"
-LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
-UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-SYMBOL = "!#$%&*+-=?@^_`|"
-AMBIGUOUS = "{}[]()/\\\'\"`~,;:.<>"
+NUMBER = string.digits
+LOWERCASE = string.ascii_lowercase
+UPPERCASE = string.ascii_uppercase
+SYMBOL = string.punctuation
 
 
 def main():
@@ -43,18 +43,12 @@ def main():
             args.verbose and print("Including uppercase letters: {0}".format(UPPERCASE))
             characters += UPPERCASE
 
-        if args.ambiguous:
-            args.verbose and print("Including ambiguous symbols: {0}".format(AMBIGUOUS))
-            characters += AMBIGUOUS
-
         args.verbose and print("Password(s) will be generated from these characters:\n\n{0}\n".format(characters))
 
     args.verbose and print(f"Generating {args.number} set{'s'[:args.number^1]} of password.\n")
 
     for _ in range(args.number):
-        password = ""
-        for _ in range(args.length):
-            password += choice(characters)
+        password = ''.join(choice(characters) for _ in range(args.length))
         print(password)
         copy(password)
 
@@ -67,7 +61,6 @@ def addArgument():
     parser.add_argument("--excludeNumber", action='store_true', help="Excludes numbers")
     parser.add_argument("--excludeLowercase", action='store_true', help="Excludes lowercase letters")
     parser.add_argument("--excludeUppercase", action='store_true', help="Excludes uppercase letters")
-    parser.add_argument("-a", "--ambiguous", action='store_true', help="Includes ambiguous symbols")
     parser.add_argument("-n", "--number", type=int, help="Numbers of password to be generated", default=1)
     parser.add_argument("-c", "--custom", type=str, help="Provides customized characters as a string")
     parser.add_argument("-v", "--verbose", action='store_true', help="Run the program in verbose mode")
